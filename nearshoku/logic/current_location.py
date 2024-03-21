67efe40934d1248bafb5ee333311cea5f7971bfe
op6160 <sagvd01@gmail.com>
2024-03-22 03:07:56 +0900

    feat: Add showing location(lat,lng) in index

--- nearshoku/logic/current_location.py
+++ nearshoku/logic/current_location.py
@@ -1,12 +1,18 @@
-from dotenv import load_dotenv
-import os
 import json
 import requests
 
+def get_api(api_type):
+    from dotenv import load_dotenv
+    import os
 
-def get_latlng(API_KEY):
-    API_HOST = 'https://www.googleapis.com/geolocation/v1/'
-    url = f'{API_HOST}geolocate?key={API_KEY}'
+    load_dotenv()
+    api_key = os.environ.get(api_type)
+    return api_key
+
+
+def get_latlng(api_key):
+    api_host = 'https://www.googleapis.com/geolocation/v1/'
+    url = f'{api_host}geolocate?key={api_key}'
 
     response = json.loads(requests.post(url).text)
 
@@ -16,17 +22,14 @@ def get_latlng(API_KEY):
     return lat, lng
 
 
-def get_location(lat, lng, API_KEY):
-    API_HOST = 'https://maps.googleapis.com/maps/api/geocode/'
-    url = f'{API_HOST}json?latlng={lat},{lng}&key={API_KEY}'
+def get_location(lat, lng, api_key):
+    api_host = 'https://maps.googleapis.com/maps/api/geocode/'
+    url = f'{api_host}json?latlng={lat},{lng}&key={api_key}'
     response = json.loads(requests.post(url).text)
-    print(response)
-    location=1
-    return location
+    return response
 
-load_dotenv()
-google_maps_api = os.environ.get('GEOLOCATION_API_KEY')
 
+google_maps_api = get_api('GEOLOCATION_API_KEY')
 lat, lng = get_latlng(google_maps_api)
 print(lat,lng)
 location = get_location(lat, lng, google_maps_api)
--- nearshoku/nearshoku/views.py
+++ nearshoku/nearshoku/views.py
@@ -1,4 +1,10 @@
+from logic.const import Const
 from django.shortcuts import render
+from logic.current_location import get_latlng, get_location, get_api
+
+CONST = Const()
 
 def index(request):
-    return render(request, 'index.html')
+    api_key = get_api(CONST.GOOGLE_API)
+    lat, lng = get_latlng(api_key)
+    return render(request, 'index.html',{'lat':lat, 'lng':lng})
