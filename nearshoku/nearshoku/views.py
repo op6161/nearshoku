77bcf75491457eba644c484e11d7a0c35a36c27a
op6160 <sagvd01@gmail.com>
2024-03-22 03:15:05 +0900

    fix: Edit current lat,lng vars name more intuitively

--- nearshoku/nearshoku/views.py
+++ nearshoku/nearshoku/views.py
@@ -4,7 +4,8 @@ from logic.current_location import get_latlng, get_location, get_api
 
 CONST = Const()
 
+
 def index(request):
     api_key = get_api(CONST.GOOGLE_API)
-    lat, lng = get_latlng(api_key)
-    return render(request, 'index.html',{'lat':lat, 'lng':lng})
+    current_lat, current_lng = get_latlng(api_key)
+    return render(request, 'index.html',{'current_lat':current_lat, 'current_lng':current_lng})
