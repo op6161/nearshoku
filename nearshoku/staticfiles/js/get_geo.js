3d34ca9dad7eb045594cdb35eb3a642491f0ad35
op6160 <sagvd01@gmail.com>
2024-03-29 03:11:07 +0900

    refactor: edit js script sequence dependency

--- nearshoku/staticfiles/js/get_geo.js
+++ nearshoku/staticfiles/js/get_geo.js
@@ -1,18 +1,19 @@
 function init()
 {
     window.navigator.geolocation.getCurrentPosition(get_latlng);
+    console.log("init_get_geo");
 }
-
+/**
+ *
+ * @param position
+ */
 function get_latlng(position)
 {
+    console.log("get_latlng");
     let lat = position.coords.latitude;
     let lng = position.coords.longitude;
-    // $('#current_lat_geo').text(lat)
-    // $('#current_lng_geo').text(lng)
-    $('#current_lat').val(lat)
-    $('#current_lng').val(lng)
-    $('#current_lat_s').val(lat)
-    $('#current_lng_s').val(lng)
+    // window.lat = lat
+    // window.lng = lng
 }
 
 window.addEventListener('load', init);
--- nearshoku/staticfiles/js/index.js
+++ nearshoku/staticfiles/js/index.js
@@ -1,30 +1,85 @@
-var selected_lat,selected_lng;
+/**
+* A function init google map
+ * set center to received location
+ * set pin to center
+ * map click event : place new pin, move screen center to pin, set selected location
+* requires google map api script
+*/
+function initMap() {
+    // 35.6812362,139.7671248
+    // test value Tokyo Station
+    var selected_lng
+    var selected_lat
 
-function init_index(){
-    window.navigator.geolocation.getCurrentPosition(initMap)
+    navigator.geolocation.getCurrentPosition(async function(position){
+        // var latlng = new google.maps.LatLng(35.6812362, 139.7671248)
+
+        var latlng = await new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+        const map = await new google.maps.Map(document.getElementById("map"), {center: latlng, zoom: 13,});
+        var marker = await new google.maps.Marker({position: latlng, map: map,});
+
+
+
+        function get_latlng(latlng) {
+            var lat = latlng['lat']
+            var lng = latlng['lng']
+            $('#current_lat').val(lat)
+            $('#current_lng').val(lng)
+            // by selected
+            $('#current_lat_s').val(lat)
+            $('#current_lng_s').val(lng)
+        }
+        /**
+         * map click event
+          * place new pin
+          * move screen center to pin
+          * set selected location
+         */
+        map.addListener("click", function (e) {
+            marker.setMap(null);
+            placeMarkerAndPanTo(e.latLng, map);
+            selected_lat = e.latLng.lat();
+            selected_lng = e.latLng.lng();
+            set_select(selected_lat, selected_lng)
+        })
+        /**
+         * place marker and move screen center to marker
+         * @event click
+         * @param {dict} latLng latlng dictionary
+         * @param {google.maps.Map} map google map object
+         */
+        function placeMarkerAndPanTo(latLng, map) {
+            // set marker on clicked
+            marker = new google.maps.Marker({position: latLng, map: map,});
+            // move screen to set marker
+            map.panTo(latLng);
+        }
+    })
 }
 
-async function initMap(position)
-{
-    let latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-    const map = new google.maps.Map(document.getElementById("map"), {center:latlng, zoom: 13,});
-    var marker = new google.maps.Marker({position: latlng, map: map,});
 
-    map.addListener("click",function(e) {
-        marker.setMap(null);
-        placeMarkerAndPanTo(e.latLng, map);
-        selected_lat = e.latLng.lat();
-        selected_lng = e.latLng.lng();
-        $("#selected_lat").val(selected_lat);
-        $("#selected_lng").val(selected_lng);
-        $("#submitSelectedLocation").attr('disabled',false);
-        $("#submitSelectedLocation").val("Search By Selected Location");
-    })
 
-    function placeMarkerAndPanTo(latLng, map) {
-        marker = new google.maps.Marker({position: latLng, map: map,});
-        map.panTo(latLng);// move screen to marker
-    }
-};
+/**
+ * A function set selected location data to send views
+  * set selected location
+  * active by selected search button
+ * @param {number} lat selected latitude
+ * @param {number} lng current longitude
+ */
+function set_select(lat, lng){
+    $("#selected_lat").val(lat);
+    $("#selected_lng").val(lng);
+    $("#submitSelectedLocation").attr('disabled',false);
+    $("#submitSelectedLocation").val("Search By Selected Location");
+}
 
-window.addEventListener("load", init_index);
+/**
+ * A function set current location data to send views
+ * @param {number} lat current latitude
+ * @param {number} lng current longitude
+ */
+
+function set_current(){
+    window.navigator.geolocation.getCurrentPosition(get_latlng)
+
+}
