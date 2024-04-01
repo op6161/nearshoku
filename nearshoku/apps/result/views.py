6baf7d185549d40f4f3c97db21f2830f2b323cb0
op6160 <sagvd01@gmail.com>
2024-04-01 19:17:48 +0900

    doc: Write docstring

--- nearshoku/apps/result/views.py
+++ nearshoku/apps/result/views.py
@@ -8,27 +8,39 @@ from .views_module import model_form_save
 
 # settings, modules
 def constant(func):
-    """
-    A decorator function for _Const Class
+    """A decorator function for creating constant properties in a class.
+
+    This decorator is intended to be used with a class '_Const' to create constant properties.
+    It ensures that the decorated property can not be modified after initialization.
+
+    Args:
+        func: function: The function that returns the constant value.
+
+    Returns:
+        property: A property object that acts as a constant property.
     """
     def func_set(self, value):
-        """
-        you can't edit constant
-        """
+        """Raises TypeError when attempting to set the constant property."""
         raise TypeError
 
     def func_get(self):
-        """
-        you can use constant
-        """
+        """Returns the constant value."""
         return func()
     return property(func_get, func_set)
 
 
 class _Const(object):
+    """A container for constant values.
+
+    This class provides a convenient way to define and access constant values.
+    Constants are defined as properties using the @constant decorator,
+    ensuring that they cannot be modified after initialization.
+
+    Example:
+        GOOGLE_API: str = _Const.GOOGLE_API
+        NOT_FOUND: str = _Const.BAD_REQUEST
     """
-    save constants
-    """
+
     @constant
     def GOOGLE_API():
         return 'GEOLOCATION_API_KEY'
@@ -59,10 +71,11 @@ def parsing_xml_to_json(xml_data):
     Parse the xml data into json format
 
     Args:
-         xml_data:requests.get()(xml-format):
+         xml_data :requests.Response : The XML data obtained from a web request.
 
     Returns:
-        json_data:requests.get()(json format).text:
+        str: JSON-formatted data converted from the XML input.
+        (requests.Response.text() json format)
     """
     import xmltodict
     import json
@@ -72,13 +85,16 @@ def parsing_xml_to_json(xml_data):
     return json_data
 
 
-def check_u3000(item):
-    """Replace all u3000 to ' ' (space) in the string
+def check_u3000(item, exception = False):
+    """Replace all '\u3000' to ' ' (space) in the input string(and list, dict).
 
     it works left-side first
 
     Args:
         item: str, list, dict, None: the string(s) that has unicode u3000
+            If 'item' is a list or dict, its elements should be type of str or None.
+        exception (bool, optional): If True, exceptions will be raised when encountering errors during replacement.
+            Defaults to False.
 
     Raises:
         ValueError: if the item is invalid type
@@ -91,7 +107,16 @@ def check_u3000(item):
     elif isinstance(item, list):
         temp_list = []
         for text in item:
-            temp_list.append(text.replace('\u3000', ' '))
+            if exception:
+                try:
+                    temp_list.append(text.replace('\u3000', ' '))
+                except Exception:
+                    raise Exception
+            else:
+                try:
+                    temp_list.append(text.replace('\u3000', ' '))
+                except:
+                    temp_key.append(text)
         return templist
     elif item is None:
         return None
@@ -99,24 +124,58 @@ def check_u3000(item):
         temp_keys = []
         temp_vals = []
         for key, val in item.items():
-            tenp_keys.append(key.replace('\u3000', ' '))
-            temp_vals.append(val.replace('\u3000', ' '))
+            if exception:
+                try:
+                    temp_keys.append(key.replace('\u3000', ' '))
+                    temp_vals.append(val.replace('\u3000', ' '))
+                except Exception:
+                    raise Exception
+            else:
+                try:
+                    temp_keys.append(key.replace('\u3000', ' '))
+                    temp_vals.append(val.replace('\u3000', ' '))
+                except Exception:
+                    temp_keys.append(key)
+                    temp_vals.append(val)
         return dict(zip(temp_keys, temp_vals))
     else:
         raise ValueError(f'invalid type{type(item).__name__}')
 
 
+def check_list_has_blank(list_data):
+    """Remove blank elements from the input list
+
+        Args:
+            list_data: list: The list to be checked for blank elements.
+
+        Raises:
+            TypeError: If list_data is not a list.
+
+        Returns:
+            list: The list with blank elements removed.
+    """
+    if not isinstance(list_data, list):
+        raise TypeError('list_data must be a list')
+    temp_list_data = []
+    for data in list_data:
+        if data != '':
+            temp_list_data.append(data)
+    return temp_list_data
+
+
 def combine_dictionary(dict1, dict2):
     """Combine two dictionaries. but they should have different keys
 
-    * if dicts have same keys, it can update dict1's value without combining
+    * If the two dicts have same keys, values from dict2 will update
+    the corresponding values in dict1.
+     * If the dictionaries have different keys, the function will simply merge them.
 
     Args:
         dict1: dict
         dict2: dict
 
     Returns:
-         dict: dict
+         dict: A dictionary containing the combined key-value pairs from both dictionaries.
     """
     dict1.update(dict2)
     return dict1
@@ -125,8 +184,8 @@ def combine_dictionary(dict1, dict2):
 class InfoProcessor:
     """A class to split and replace string
 
-    This class provides methods to split a string based on a specified symbol
-    or to replace specific values in a given string with another value.
+    This class provides methods to split a string based on a specified symbol or
+     to replace specific values in a given string with another value.
 
         Examples:
         - To split a string:
@@ -146,11 +205,12 @@ class InfoProcessor:
 
         Args:
             string :str : The string to be processed
-            mode :str: The processing mode. Possible values are "split", "replace", "all"
-            to_value :str: The value to replace or the symbol to split on
-            from_value :str,list (optional): The target to replace or the symbols to split on
-            replace_ex :dict (optional): A dictionary for exception handling
-            unicode_check :bool (optional): Whether to check for Unicode
+            mode :str: The processing mode. Possible values are "split", "replace", "all".
+            to_value :str: The value to replace or the symbol to split on.
+            from_value :str,list (optional): The target to replace or the symbols to split on.
+                if you want to 'split', this argument is essential.
+            replace_ex :dict (optional): A dictionary for exception handling.
+            unicode_check :bool (optional): Whether to check for Unicode.
 
         Raises:
             ValueError: If the input parameters are invalid or if required parameters are missing
@@ -372,19 +432,21 @@ def err_direct(request, state=500):
     """Handle direct error response
 
     Args:
-        request: HttpRequest
+        request: HttpRequest: Django HTTP request
         state: int(optional): state code received from logical functions
 
     Returns:
         HttpResponse: The rendered error response page
     """
-    # ----------------------------------------------------------------
+
     def err_check(check_state):
         """Internal function to check error code
 
-        Args: check_state: int: state code received from logical functions
+        Args:
+            check_state: int: state code received from logical functions
 
-        Returns: str: error message
+        Returns:
+            str: error message
         """
         if check_state == 400:
             return CONST.BAD_REQUEST,
@@ -414,30 +476,59 @@ def err_direct(request, state=500):
 
 # views
 def index(request):
-    '''
-    apps.result.index
-    '''
+    """View function to render 'index' of 'result' app
+
+    In this function, retrieve the GOOGLE_MAP_API key using get_key()
+    and pass it to result_index template for rendering.
+    Then the result_index page load google-maps to select location to search
+
+    Args:
+        request: HttpRequest: Django HTTP request
+    Returns:
+        HttpResponse: Render 'result_index' page of 'result app'
+    """
     api_key = get_key(CONST.GOOGLE_API)
     contexts = {'api_key': api_key}
     return render(request, 'result_index.html', contexts)
 
 
 def result(request):
-    '''
-    apps.result.result(search-result)
+    """View function to render 'search result' of 'result' app
 
-    Raise:
-        400 Bad Request: Get invalid request method
-        404 Not Found: No search result
+    result() function handles both 'GET' and 'POST' requests.
+    For 'POST' request, it processes the form data to retrieve search-results
+        from hot-pepper-API and stores them in the session.
+    For 'GET' request, it retrieves the stored search-results by API from session
+        and paginate them to display on template.
+    If the API call is unsuccessful, calls err_direct() to render 'result_error'
 
     Return:
-        request to result_show()
-    '''
+        HttpResponse:
+            result: Render 'result' page of 'result app'
+            400 Bad Request: Render 'result_error' page of 'result app' when an invalid request method is received
+            404 Not Found: Render 'result_error' page of 'result app' when no search results received by API server.
+    """
 
-    # ----------------------------------------------------------------
     def post_shop_info(request):
-        """ """
+        """Internal function to process POST request and retrieve search results from hot-pepper-API
+
+        post_shop_info() retrieves param data from POST request, send a request to hot-pepper-API to retrieve shop data.
+        And store them in session.
+
+        Args:
+            request: HttpRequest: Django HTTP request
+
+        Raises:
+            ValueError: If the request method is not POST.
+
+
+        Returns:
+            int: A state code of hot_pepper_api() function
+                - 200: If the API call was successful
+                - others: If there was an error
+        """
         if request.method == 'POST':
+            request.session.flush()
             range_ = request.POST['range_select']
             order = request.POST['order_select']
             current_lng = request.POST['current_lng']
@@ -462,30 +553,42 @@ def result(request):
             if api_state != 200:
                 return api_state
 
-            shop_info = info_pack(shop_info_json, SHOP_INFO_MODEL_FORM, search_lat, search_lng)
+            shop_info = info_pack(shop_info_json, SHOP_INFO_MODEL_FORM)
             request.session['shop_info'] = shop_info
-            return 200
-    # ----------------------------------------------------------------
-    def result_method_check(request):
-        """ """
-        if request.method == 'POST':
-            request.session.flush()
-            db_state = post_shop_info(request)
-            db_page = None
-        elif request.method == 'GET':
-            db_state = 200
-            db_page = request.GET.get('page')
+            return api_state
         else:
-            db_state = 400
-            db_page = None
+            raise ValueError('post_shop_info() requires request.method POST')
+    # ----------------------------------------------------------------
 
-        return db_state, db_page
+    def result_method_check(request):
+        """Internal function to check the request method and dispatch the processing accordingly.
+
+        result_method_check() examines the request method and dispatches the processing accordingly.
+        If request method is 'POST', it calls post_shop_info() to process the POST request.
+        If request method is 'GET', it sets the function state to 200 and retrieved 'page' to proceed result() process.
+
+        Args:
+            request: HttpRequest: Django HTTP request
+
+        Returns:
+            Tuple(int, str): A tuple containing the function state and page parameter
+         """
+        if request.method == 'POST':
+            f_state = post_shop_info(request)
+            request_page = None
+        elif request.method == 'GET':
+            f_state = 200
+            request_page = request.GET.get('page')
+        else:
+            f_state = 400
+            request_page = None
+
+        return f_state, request_page
     # ----------------------------------------------------------------
 
     state, page = result_method_check(request)
 
     if state != 200:
-        error_message = err_check(state)
         return err_direct(request, state)
     elif request.method == 'POST':
         return redirect('result')
@@ -509,15 +612,18 @@ def result(request):
 
 
 def detail(request):
-    '''
-    apps.result.detail(shop-detail-info)
+    """View function to render 'search_detail' of 'result' app
 
-    Raises:
-        400 Bad Request: Get invalid request method
+    detail() handles GET request to retrieve detailed information about a shop
+    from hot-pepper-API based on the provided shop-id.
+    If the API call is successful, it renders 'result_detail' page with retrieved information of 'result app'.
+    If the API call is unsuccessful, calls err_direct() to render 'result_error'.
 
     Returns:
-        render
-    '''
+        HttpResponse:
+            result_detail: Render 'result_detail' page of 'result app'.
+            err_direct: Render 'result_error' page of 'result app'.
+    """
     if request.method == 'GET':
         shop_id = request.GET.get('shop_id')
         detail_info_json, state = hot_pepper_api(id=shop_id)
@@ -533,23 +639,52 @@ def detail(request):
         return err_direct(request, 400)
 
 
-def info_pack(info_json, model_form, lat=None, lng=None):
-    '''
-    Pack information to show page
+def info_pack(info_json, model_form):
+    """Pack information to prepare display on the page
+
+    info_pack() takes a list of dicts retrieved from hot-pepper-API shop information response['result']['shop'] format.
+    Process that using InfoProcessor class, and formats it according to the specified data format.
+
     Args:
         info_json: list[dict]: hotpepper_response['results']['shop'] format dicts list
-        model_form: django.models.ModelForm object
-        lat(optional): float: searched latitude
-        lng(optional): float: searched longitude
+            A list of dictionaries representing shop information.
+        model_form: django.models.ModelForm: The Django model form object to
+            determine the format of the output information.
 
     Returns:
-        list[dict{}]
-    '''
+        list[dict]: A list of dictionaries of shop information formatted for template variables name to
+            be displayed on the page.
+    """
 
-    # ----------------------------------------------------------------
     def use_info_processor(info_json, key, mode, to_value=None, from_value=None,
                            replace_ex=None, unicode_check=True, ):
-        """Use InfoProcessor class for processing information in this project"""
+        """Use InfoProcessor class to process information in this project
+
+        use_info_processor() is an internal function to apply the InfoProcessor class to
+        process information in the project.
+        It iterates over each shop dictionary in the info_json(list),
+        extracts the specified key from the hot-pepper-API, processes the value using the InfoProcessor class and
+        updates the value in the shop dictionary with the processed result.
+        Finally, it returns the list of shop dictionaries with processed information.
+
+        * If the mode is 'all' or 'split', the value is converted from str to list.
+
+        Args:
+            info_json: list[dict]: A list of dictionaries of shop information.
+            key: str: The key to extract from shop dictionary.
+            mode: str: The processing mode for InfoProcessor.
+                Possible values ar "split", "replace", "all"
+            to_value: str(optional): The value to replace or the symbol to split on.
+            from_value: str, list(optional): The target to replace or the symbol to split on.
+            replace_ex: dict(optional): A dictionary for exception replace handling.
+            unicode_check: (bool, optional): Check u3000 to replace ' '(space). Default is True.
+
+        Returns:
+            list[dict]: A list of dictionaries processed shop information
+
+        """
+
+
         temp_shop_list = []
         for shop in info_json:
             temp_shop = shop
@@ -559,7 +694,8 @@ def info_pack(info_json, model_form, lat=None, lng=None):
                                            from_value=from_value,
                                            replace_ex=replace_ex,
                                            unicode_check=unicode_check, )
-            temp_shop[key] = info_processor.result
+            # temp_shop[key] = info_processor.result
+            temp_shop[key] = check_list_has_blank(info_processor.result)
             temp_shop_list.append(temp_shop)
             del info_processor
         return temp_shop_list
@@ -581,13 +717,13 @@ def info_pack(info_json, model_form, lat=None, lng=None):
                                        from_value='）')
         for shop in info_json:
             model_template = {
-                # 필수요구
+                # 필수
                 'detail_shop_id': shop['id'],
                 'detail_name': check_u3000(shop['name']),
                 'detail_address': check_u3000(shop['address']),
                 'detail_image': shop['photo']['pc']['l'],
                 'detail_time': shop['open'],
-                # + info
+                # 추가
                 'detail_kana': check_u3000(shop['name_kana']),
                 'detail_access_list': shop['access'],
                 'detail_shop_memo': check_u3000(shop['shop_detail_memo']),
@@ -611,11 +747,10 @@ def info_pack(info_json, model_form, lat=None, lng=None):
                 'shop_kana': check_u3000(shop['name_kana']),
                 'shop_access_list': shop['access'],
                 'shop_thumbnail': shop['logo_image'],
-                'searched_lat': lat,
-                'searched_lng': lng,
             }
             info_package.append(model_template)
 
     return info_package
 
-### test
+
+#  test codes  #
